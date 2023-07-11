79f31960f56111fec7ab9da9b8606a347d3c0f56
inomata137 <inomata-kazuki137@g.ecc.u-tokyo.ac.jp>
2023-07-11 14:56:03 +0900

    use built-in random module

--- src/transformer/common/random_choice.py
+++ src/transformer/common/random_choice.py
@@ -1,10 +1,12 @@
 import numpy
+from random import Random
 
 class RandomChoiceGenerator:
     def __init__(self, seed=2023):
-        self.rng = numpy.random.default_rng(seed)
+        self.gen = Random(seed)
     
     def choice(self, p):
         if type(p) is not numpy.ndarray:
+            # expect cupy.ndarray
             p = p.get()
-        return int(self.rng.choice(len(p), 1, p=p)[0])
+        return self.gen.choices(range(len(p)), p, k=1)[0]
